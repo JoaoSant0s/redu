a2474c5539e083ca87a70dff80147d277950f87c
Lucas Biagini <lbs4@cin.ufpe.br>
2015-04-18 20:16:21 -0300

    steps dos meus primeiros dois cenarios

--- cucumber/steps/FreeEnrollmentPlanSteps.rb
+++ cucumber/steps/FreeEnrollmentPlanSteps.rb
@@ -1,3 +1,5 @@
+#igor lira
+#Scenario: Notificar os usuarios sobre a mudança na assinatura de um curso de "Gratuito" para "Mensal"
 Given(/^que um curso "([^"]*)" tem assinatura gratuita $/) do |nome_curso|
 	curso = Course.find(:name => nome_curso)
 	assert_equal(curso.enrollment_type, 'free')
@@ -24,8 +26,8 @@ And(/^notifica todos os usuários cadastrados no curso "([^"]*)" sobre a modific
 end
 
 
-
-
+#igor lira
+#Scenario: opção de tornar o curso com “Assinatura Mensal” para “Assinatura Gratuita” para todos os usuários cadastrados no curso
 Given(/^estou me cadastrando no curso "([^"]*)"$/) do |nome_curso|
 	curso = Course.find(:name => nome_curso)
 	assert_equal(current_page, 'course_enrollment')
@@ -45,4 +47,55 @@ end
 Then(/^minha inscrição no curso "([^"]*)" é automaticamente aprovada$/) do |nome_curso|
 	curso = Course.find(:name = nome_curso)
 	curso.join! my_user
+end
+
+#lucas biagini
+#Scenario: Modificar assinatura de curso de “Mensal” para “Gratuito”
+Given (/^estou na página de configurações da assinatura do curso "([^"]*)"$/) do |nome_curso|
+	curso = Course.find(:name = nome_curso)
+	assert_equal(current_page, 'enrollment_type')
+end
+
+When (/^eu seleciono a opção de assinatura do curso "([^"]*)" para “Gratuito”$/) do |nome_curso|
+	curso = Course.find(:name = nome_curso)
+	curso.set_enrollment_plan 'free'
+end
+
+And (/^eu confirmo as alterações no curso "([^"]*)"$/) do |nome_curso|
+	curso = Course.find(:name = nome_curso)
+	curso.set_enrollment_plan! 'free'
+end
+
+Then (/^o tipo da assinatura do curso "([^"]*)" é alterado para “Gratuito”$/) do |nome_curso|
+	curso = Course.find(:name = nome_curso)
+	curso.assert_equal(enrollment_type, 'free')
+end
+
+#lucas biagini
+#Scenario: Modificar assinatura de curso de “Mensal” para “Gratuita”, dado que escolheu preço zero
+Given (/estou na página de configurações da assinatura do curso "([^"]*)"/) do |nome_curso|
+	curso = Course.find(:name = nome_curso)
+	assert_equal(current_page, 'enrollment_type')
+end
+
+And (/o curso "([^"]*)" é um curso com assinatura "Mensal"/) do |nome_curso|
+	curso = Course.find(:name = nome_curso)
+	curso.set_enrollment_plan 'monthly'
+end
+
+When (/eu preencho com "[^0]" o valor do curso "([^"]*)"/) do |nome_curso|
+	curso = Course.find(:name = nome_curso)
+	curso.set_value '0'
+end
+
+And (/o sistema me pergunta se eu quero alterar o tipo de assinatura do curso "([^"]*)" para "Gratuito"/) do |nome_curso|
+	curso = Course.find(:name = nome_curso)
+	if system.confirm(:option, 'true') do
+		curso.set_enrollment_plan 'free'
+	end
+end
+
+Then (/o tipo de assinatura do curso "([^"]*)" é alterado para “Gratuito”/) do |nome_curso|
+	curso = Course.find(:name = nome_curso)
+	curso.assert_equal(enrollment_type, 'free')
 end
